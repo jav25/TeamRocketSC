d2cf00749fe00846ee5441fb81d585ba00160eb8
jav25 <jav@email.sc.edu>
2017-11-01 16:52:41 -0400

    added the new scene

--- Assets/_MyAssets_/Scripts/CoverManager.cs
+++ Assets/_MyAssets_/Scripts/CoverManager.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CoverManager : MonoBehaviour {
+public class CoverManager : MonoBehaviour
+{
 
     public Collider player1;
     public Collider player2;
@@ -13,9 +14,11 @@ public class CoverManager : MonoBehaviour {
     public Vector3 directionV;
     public int direction;
 
-    public int p1 = 0;
-    public int p2 = 1;
-    public int p3 = 2;
+    public int p1;
+    public int p2;
+    public int p3;
+
+    public int p = -1;
 
     public string parentTag;
 
@@ -61,24 +64,36 @@ public class CoverManager : MonoBehaviour {
     //try making another function that does all the stuff inside if(!enter) but by using collider other to get what the char id is
     //and then find a way to ignore that collider if another one comes in
 
+    public void FlipCharacter()
+    {
+        if (temp.gameObject.GetComponent<UnitTurn>().charId == 0 && player1.gameObject.GetComponent<PlayerController>().trigger != true)
+        {
+            player1.gameObject.GetComponent<PlayerController>().trigger = true;
+        }
+        else if (temp.gameObject.GetComponent<UnitTurn>().charId == 1)
+        {
+            Debug.Log("WHAT THE FUCK");
+            player2.gameObject.GetComponent<PlayerController>().trigger = true;
+        }
+    }
+
+
     public void OnTriggerEnter(Collider other)
     {
         if (!enter)
         {
-            if (player1.gameObject.GetComponent<UnitTurn>().charId == 0 && p1 == 0)
+            if (player1.gameObject.GetComponent<UnitTurn>().charId == 0 && player1.gameObject.GetComponent<PlayerController>().trigger != true)
             {
                 player1.gameObject.GetComponent<PlayerController>().trigger = true;
-                p1 = -1;
+                //enter = true;
             }
-            else if (player2.gameObject.GetComponent<UnitTurn>().charId == 1 && p2 == 1)
+            else if (player2.gameObject.GetComponent<UnitTurn>().charId == 1 && player2.gameObject.GetComponent<PlayerController>().trigger != true)
             {
                 player2.gameObject.GetComponent<PlayerController>().trigger = true;
-                p2 = -1;
             }
-            else if (player3.gameObject.GetComponent<UnitTurn>().charId == 2 && p3 == 2)
+            else if (player3.gameObject.GetComponent<UnitTurn>().charId == 2 && player3.gameObject.GetComponent<PlayerController>().trigger != true)
             {
                 player3.gameObject.GetComponent<PlayerController>().trigger = true;
-                p3 = -1;
             }
         }
     }
